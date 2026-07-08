de369a340a7193ac7cd57dfa7b8e054985405449
Ali Hassnain <ali.hassnain0365@gmail.com>
2026-07-08 08:05:41 -0700

    DAY-5-5.2- Initialized the list

---
+++ DAY_5/2.py
@@ -0,0 +1,13 @@
+# declaring list with more than 5 items
+
+# way number 1
+
+l1 = list("Ali") # in this way i could create a list that would contain a character by character 
+# like it would be l1 = ['A','L','I'] -> iterable
+
+print("My Life : ",l1)
+
+# the way number 2
+
+l2 = ['ahsan', 'sheki','abby butt', 'daniyal', 'sadi','ayan']
+print('My Friends : ', l2)
